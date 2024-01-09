cd59234e089d64d46d408a5cfb23e9386f917f26
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2024-01-09 08:06:15 +0000

    feat: added users app (#1)
    * feat: added users app
    
    * chore: code format

---
+++ apps/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    username = None
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        help_text=_("Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
