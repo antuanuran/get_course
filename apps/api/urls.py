a85f976fbb3f00f8c578e8b4e531f98bfac52c43
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2024-01-30 10:29:49 +0300

    fix: removed old view

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from apps.api.views.courses import CourseViewSet, import_file
+from apps.api.views.courses import CourseViewSet
 
 router = DefaultRouter()
 router.register("courses", CourseViewSet)
@@ -10,6 +10,5 @@ router.register("courses", CourseViewSet)
 urlpatterns = [
     path("", include("djoser.urls")),
     path("", include(router.urls)),
-    path("import/", import_file),
     path("auth/", include("djoser.urls.jwt")),
 ]
