2ee74561eeb39180e616bb951ba9b388c3539013
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2024-02-01 11:04:22 +0300

    feat: added favourites API

--- apps/api/views/courses.py
+++ apps/api/views/courses.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from apps.api.serializers.courses import CourseSerializer
 from apps.api.views.base import BaseModelViewSet
@@ -13,15 +13,6 @@ class CourseViewSet(BaseModelViewSet):
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    filter_backends = [
-        DjangoFilterBackend,
-        OrderingFilter,
-        SearchFilter,
-    ]
-
-    filterset_fields = ["product"]
-    # search_fields = []
-
     def get_queryset(self, queryset=None):
         filter_params = Q(is_sellable=True)
         user = self.request.user
@@ -31,3 +22,14 @@ class CourseViewSet(BaseModelViewSet):
         # TODO: make annotations
         # qs = qs.annotate(is_purchased=Exists("purchased_courses"))
         return qs
+
+    @action(detail=True, methods=["post"], url_path="toggle-favourite", permission_classes=[IsAuthenticated])
+    def toggle_favourite(self, request, *args, **kwargs):
+        course = self.get_object()
+        user = request.user
+        if course.favourites.filter(id=user.id).exists():
+            course.favourites.remove(user)
+        else:
+            course.favourites.add(user)
+        serializer = self.get_serializer(instance=course)
+        return Response(serializer.data)
