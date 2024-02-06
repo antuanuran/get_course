531f1b106055dc3eebb31611b7d87178e7022b6e
Anton <115862820+antuanuran@users.noreply.github.com>
2024-02-06 09:27:57 +0200

    feat: add swagger (#12)
    * feat: add swagger
    
    * chore: minor fixes
    
    * typo
    
    ---------
    
    Co-authored-by: Alexander Ivanov <oz.sasha.ivanov@gmail.com>

--- apps/api/views/courses.py
+++ apps/api/views/courses.py
@@ -1,28 +1,46 @@
 from django.db.models import Q
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import no_body, swagger_auto_schema
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
+from apps.api.permissions import IsAuthorOrReadOnly
 from apps.api.serializers.courses import CourseSerializer
 from apps.api.views.base import BaseModelViewSet
 from apps.courses.models import Course
 
 
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(
+        operation_description="Для вывода полного списка курсов - авторизация не обязательна",
+        security=[],
+    ),
+)
+@method_decorator(
+    name="retrieve",
+    decorator=swagger_auto_schema(
+        operation_description="Для вывода конкретного курса - авторизация не обязательна",
+        security=[],
+    ),
+)
 class CourseViewSet(BaseModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self, queryset=None):
         filter_params = Q(is_sellable=True)
         user = self.request.user
         if user.is_authenticated:
-            filter_params |= Q(purchases__user=user) | Q(favourites=user)
+            filter_params |= Q(purchases__user=user) | Q(favourites=user) | Q(author=user)
         qs = super().get_queryset(queryset).filter(filter_params)
         # TODO: make annotations
         # qs = qs.annotate(is_purchased=Exists("purchased_courses"))
         return qs
 
+    @swagger_auto_schema(request_body=no_body)
     @action(detail=True, methods=["post"], url_path="toggle-favourite", permission_classes=[IsAuthenticated])
     def toggle_favourite(self, request, *args, **kwargs):
         course = self.get_object()
