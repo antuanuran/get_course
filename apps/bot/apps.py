115b3e24f62f53c7a9fb8e73fa82f8c34aefdad8
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2024-04-23 11:01:44 +0300

    start logic

--- apps/bot/apps.py
+++ apps/bot/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BotConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.bot"
+
+    def ready(self):
+        from apps.bot.service import bot
+
+        bot.delete_webhook()
+        bot.set_webhook(url="https://skillsup.fun/api/v1/tg-updates")
